8b071c88964d966d0305c7915c7a3709742eacee
WATCHARAPOL <thegodohm28@outlook.com>
2023-12-15 15:40:35 +0700

    update for readme

--- BiomechanicsOfGait.py
+++ BiomechanicsOfGait.py
@@ -8,7 +8,7 @@ mp_pose = mp.solutions.pose
 mp_draw = mp.solutions.drawing_utils
 
 pose = mp_pose.Pose()
-cap = cv2.VideoCapture("./Dataset/gait_abnormal_10_neckpain.mp4")
+cap = cv2.VideoCapture("./Dataset/gait.mp4")
 
 # mediapipe connect point
 rl1, rl2, rl3 = 24, 26, 28
